a8cbc507e5ea4cfe32c1a5dc5c033ee59e895969
ivancrg <ivan.rubinic@outlook.com>
2022-02-03 20:00:16 +0100

    Custom size of Google tile

--- api_google_tile.py
+++ api_google_tile.py
@@ -1,4 +1,5 @@
 import requests
+import cv2 as cv
 
 # Downloads sattelite/terrain/map/hybrid
 # image (map tile  (x, y) at zoom level z)
@@ -7,7 +8,7 @@ import requests
 #                - hybrid (y)
 #                - terrain (t)
 #                - map (m)
-def getImage(image_name, x, y, z, display_opt):
+def getImage(image_name, x, y, z, display_opt, width, height):
     if display_opt != 's' and display_opt != 'y' and display_opt != 't' and display_opt != 'm':
         return
 
@@ -21,6 +22,12 @@ def getImage(image_name, x, y, z, display_opt):
 
     with open(image_name, 'wb') as handler:
         handler.write(image_data)
+    
+    # Open and resize image
+    image = cv.imread(image_name)
+    dim = (width, height)
+    image = cv.resize(image, dim, interpolation = cv.INTER_AREA)
+    cv.imwrite(image_name, image)
 
 # Testing
-# getImage("tile.png", 17, 11, 5, "m")
+# getImage("tile.png", 17, 11, 5, "m", 150, 300)
